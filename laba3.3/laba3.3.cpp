c71e3f15f0f94c42fdcee998f883c1ae6f349c89
vitaliykovalskyi <115356909+vitaliykovalskyi@users.noreply.github.com>
2022-12-18 23:44:08 +0200

    Add project files.

---
+++ laba3.3/laba3.3.cpp
@@ -0,0 +1,41 @@
+// Lab_03_3.cpp
+// Ковальський Віталій
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 11
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+	
+	// розгалуження в повній формі
+	if (x <= (-1 - R))
+		y = (-x - 2);
+	else
+		if (-1 - R < x && x <= -1)
+			y = sqrt(R * R - (x + 1) * (x + 1));
+		else
+			if (-1 < x && x <= 1)
+				y = R;
+			else
+				if (1 < x && x <= 2)
+					y = R + (- 1 - R) * (x - 1);
+				else
+					y = -1; // це поміняти
+	
+	cout << endl;
+	cout << "y = " << y << endl;
+	
+	cin.get();
+	return 0;
+}
